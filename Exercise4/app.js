8f35e833b46006c6f7bdcf59180f3b93a01e5605
linnaek <linnaekraemer@gmail.com>
2020-02-19 16:36:01 -0600

    complete exercise 4

--- Exercise4/app.js
+++ Exercise4/app.js
@@ -1,11 +1,29 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    attachEffect: false,
+    shape: 'square',
+    size: "",
+    isTrue: "",
+    color: "",
+    progress: 0,
+    length: "10px"
   },
   methods: {
     startEffect: function() {
-    
+      let that = this 
+      setInterval(()=>{
+        that.attachEffect = that.attachEffect === "highlight" ? "shrink" : "highlight"
+      }, 1000)
+    },
+    startProgress: function() {
+      let that = this
+      setInterval(()=>{
+        if(that.progress<200){
+          that.progress+=5
+          that.length = progress+"px"
+        }
+      }, 1000)
     }
   }
 });
